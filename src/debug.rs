0d60822e96aa4091901b6548327b438e82c4f8f8
Claudio Giovanni Mattera <dev@claudiomattera.it>
2021-10-08 22:24:34 +0200

    Add license header to source files

--- src/debug.rs
+++ src/debug.rs
@@ -1,3 +1,8 @@
+// Copyright Claudio Mattera 2021.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 /// Format and write text to the WASM-4 debug console
 ///
