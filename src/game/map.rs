f01177281c1a1870b9a3c8caaa4faace54b25f74
Claudio Giovanni Mattera <dev@claudiomattera.it>
2021-10-21 22:42:16 +0200

    Add function to compute neighbours

--- src/game/map.rs
+++ src/game/map.rs
@@ -170,28 +170,7 @@ impl Map {
         let neighbour_mines = self.count_neighbour_mines(mines, x, y);
         let neighbour_flags = self.count_neighbour_flags(x, y);
         if neighbour_mines == neighbour_flags {
-            // Must be a signed type to check for negative indices
-            let x = x as i32;
-            let y = y as i32;
-            vec![
-                    (x + 1, y + 1),
-                    (x + 1, y - 1),
-                    (x - 1, y + 1),
-                    (x - 1, y - 1),
-                    (x, y + 1),
-                    (x, y - 1),
-                    (x + 1, y),
-                    (x - 1, y),
-                ]
-                .into_iter()
-                .filter(|(cx, cy)| {
-                    *cx >= 0
-                    && *cy >= 0
-                    && *cx < self.width as i32
-                    && *cy < self.height as i32
-                })
-                .map(|(cx, cy)| (cx as usize, cy as usize))
-                .collect()
+            self.neighbours(x, y)
         } else {
             Vec::new()
         }
@@ -231,29 +210,41 @@ impl Map {
     }
 
     fn count_neighbour_flags(&self, x: usize, y: usize) -> usize {
-        let x = x as i32;
-        let y = y as i32;
         let mut count = 0;
 
-        let candidates = [
-            (x + 1, y + 1),
-            (x + 1, y - 1),
-            (x - 1, y + 1),
-            (x - 1, y - 1),
-            (x, y + 1),
-            (x, y - 1),
-            (x + 1, y),
-            (x - 1, y),
-        ];
-        for (cx, cy) in candidates {
-            if cx >= 0 && cy >= 0 && cx < self.width as i32 && cy < self.height as i32 {
-                if let Tile::Flagged = self.tile(cx as usize, cy as usize) {
-                    count += 1;
-                }
+        let neighbours = self.neighbours(x, y);
+        for (nx, ny) in neighbours {
+            if let Tile::Flagged = self.tile(nx as usize, ny as usize) {
+                count += 1;
             }
         }
 
         count
+    }
+
+    fn neighbours(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
+        // Must be a signed type to check for negative indices
+        let x = x as i32;
+        let y = y as i32;
+        vec![
+                (x + 1, y + 1),
+                (x + 1, y - 1),
+                (x - 1, y + 1),
+                (x - 1, y - 1),
+                (x, y + 1),
+                (x, y - 1),
+                (x + 1, y),
+                (x - 1, y),
+            ]
+            .into_iter()
+            .filter(|(cx, cy)| {
+                *cx >= 0
+                && *cy >= 0
+                && *cx < self.width as i32
+                && *cy < self.height as i32
+            })
+            .map(|(cx, cy)| (cx as usize, cy as usize))
+            .collect()
     }
 
     fn tile(&self, x: usize, y: usize) -> &Tile {
