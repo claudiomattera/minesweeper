c2c8982847032f016f5177c2de3866f0c29380c1
Claudio Giovanni Mattera <dev@claudiomattera.it>
2021-10-23 11:32:40 +0200

    Set colors when drawing elapsed time

--- src/interface.rs
+++ src/interface.rs
@@ -37,5 +37,6 @@ pub fn draw_remaining_mines_count(remaining_mines: usize, x: i32, y: i32) {
 /// Draw elapsed time box
 pub fn draw_elapsed_time(elapsed_time: u32, x: i32, y: i32) {
     let s = format!("Time:{:3}", elapsed_time);
+    DrawColors.set(0x03);
     draw_text(s, x, y);
 }
