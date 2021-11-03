e9483e7dddd9d0879185f75ea18b92d90800e1fd
Claudio Giovanni Mattera <dev@claudiomattera.it>
2021-11-03 17:45:46 +0100

    Fix warning from rustfmt

--- src/alloc.rs
+++ src/alloc.rs
@@ -5,7 +5,8 @@ const FAST_HEAP_SIZE: usize = 4 * 1024; // 4 KB
 const HEAP_SIZE: usize = 16 * 1024; // 16 KB
 const LEAF_SIZE: usize = 16;
 
-static mut FAST_HEAP: *mut [u8; FAST_HEAP_SIZE] = (0x10000 - HEAP_SIZE - FAST_HEAP_SIZE) as *mut [u8; FAST_HEAP_SIZE];
+static mut FAST_HEAP: *mut [u8; FAST_HEAP_SIZE] =
+    (0x10000 - HEAP_SIZE - FAST_HEAP_SIZE) as *mut [u8; FAST_HEAP_SIZE];
 static mut HEAP: *mut [u8; HEAP_SIZE] = (0x10000 - HEAP_SIZE) as *mut [u8; HEAP_SIZE];
 
 #[global_allocator]
